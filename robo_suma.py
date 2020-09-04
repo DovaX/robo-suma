829ba8477269bf56bd1f8bc6d271ba997204e5d9
Dominik Vach <vach.dominik@gmail.com>
2020-09-04 23:46:06 +0200

    item init

---
+++ robo_suma.py
@@ -0,0 +1,10 @@
+class Item:
+    def __init__(self,category,name,date,amount):
+        self.category=category
+        self.name=name
+        self.date=date        
+        self.amount=amount
+
+Item("buy","clothes","2020-04-09",350)
+
+list_of_items=[]
