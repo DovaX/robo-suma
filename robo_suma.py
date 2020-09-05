741faddf19a776a76379eebe1747ad816e9ced41
Dominik Vach <vach.dominik@gmail.com>
2020-09-05 02:42:01 +0200

    dogui initialization

--- robo_suma.py
+++ robo_suma.py
@@ -13,4 +13,18 @@ class Account:
 
 Item("buy","clothes","2020-04-09",350)
 
+accounts=[]
+account_names=[x.name for x in accounts]
+
 list_of_items=[]
+import dogui.dogui_core as dg
+gui1=dg.GUI("Robosuma")
+
+combo1=dg.Combobox(gui1.window,account_names,1,1)
+label1=dg.Label(gui1.window,"Aktuální stav",1,2)
+
+entry1=dg.Entry(gui1.window,1,3)
+
+label2=dg.Label(gui1.window,"",2,1)
+
+gui1.build_gui()
